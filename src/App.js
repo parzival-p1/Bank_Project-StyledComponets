de32a9a2fe4bbb603ac72f3137fd78f1b38dcef6
Paco Hdez <francj.hdez@gmail.com>
2023-05-19 19:26:24 -0600

    Migrando componentes de css a StyledComponents

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import GlobalStyle from "./GlobalStyles";
+
+import Header from "./Components/Header";
+import Container from "./Components/Container";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Container />
+    </>
+  );
+}
+
+export default App;
