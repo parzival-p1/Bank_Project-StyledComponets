ec03ac2dc0078435750b4857285532e630c64cb6
Paco Hdez <francj.hdez@gmail.com>
2023-05-23 13:45:21 -0600

    Cambio de temas e iconos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
 import GlobalStyle from "./GlobalStyles";
 import { temaClaro, temaOscuro } from "./Components/UI/temas";
 import { ThemeProvider } from "styled-components";
 import Header from "./Components/Header";
 import Container from "./Components/Container";
+import { BtnTema } from "./Components/UI"
+import SwitcherTema from "./Components/SwitcherTema";
+
 
 console.log(temaClaro, temaOscuro);
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  //* Cambio de Icono: claro / oscuro
+  const toggleTema = () => {
+    setTema(( tema ) => !tema)
+  }
+  
   return (
-    <ThemeProvider theme={ temaClaro }>
+    <ThemeProvider theme={ tema ? temaClaro : temaOscuro }>
       <GlobalStyle />
+      <BtnTema onClick={toggleTema}>
+        <SwitcherTema tema={tema}/>
+      </BtnTema>
       <Header />
       <Container />
     </ThemeProvider>
--- src/Components/SwitcherTema/index.js
+++ src/Components/SwitcherTema/index.js
@@ -3,9 +3,9 @@ import themeOn from "../../assets/images/themeOn.svg"
 import themeOff from "../../assets/images/themeOff.svg"
 import { Icono } from "../UI"
 
-export default() => {
-    const claro = <Icono src={themeOn} alt="Tema claro"/>
+export default({ tema }) => {
+    const claro = <Icono src={themeOn} alt="Tema claro" />
     const oscuro = <Icono src={themeOff} alt="Tema oscuro"/>
 
-    return <>tema ? osucro : claro</>;
+    return <> {tema ? oscuro : claro} </>;
 }
